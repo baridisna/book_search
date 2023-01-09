25f1493110a547cf2f965e8dabc6c03b69636236
baridisna <isnanurbarid@gmail.com>
2023-01-09 09:37:18 +0700

    limit 10

--- controllers/book.js
+++ controllers/book.js
@@ -7,7 +7,7 @@ export const Home = (req, res) => {
 export const Book = (req, res) => {
     const q = req.query.q
     console.log(q)
-    request(`https://www.googleapis.com/books/v1/volumes?q=${q}&maxResults=5&startIndex=0`, {json: true}, (err, response, body) => {
+    request(`https://www.googleapis.com/books/v1/volumes?q=${q}&maxResults=10&startIndex=0`, {json: true}, (err, response, body) => {
         if (err) { return console.log(err); }
         res.render('books.ejs', { books: body.items, q: q})
     })
